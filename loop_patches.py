87b285806440aea1947e9417c6e8b68238d6b031
Ivan Agyapong <ivanpagyapong@gmail.com>
2026-01-19 21:03:20 +0000

    loading json for an available device

--- loop_patches.py
+++ loop_patches.py
@@ -1,21 +1,32 @@
+
 import rtmidi
 import sounddevice
+import json
 
 class KeysInterface:
     def __init__(self):
-        self.midiout = rtmidi.RtMidiOut()
-        self.device = ""
-        self.get_device_list()
+        self.midiout = rtmidi.MidiOut()
+        self.connected_port = None
+        self.available_devices = self.get_device_list()
+        choose = input("Choose from list of devices: ").lower().replace(" ", "_")
+        profile = self.load_json(choose)
 
     def get_device_list(self):
         device_list = []
-        count = self.midiout.getPortCount()
-        i=0
-        while i < count:
-            print(f"{self.midiout.getPortName(i)} - ({i})")
-            self.device_list.append(i)
-            i+=1
+        count = self.midiout.get_port_count()
+        for i in range(count):
+            print(f"{self.midiout.get_port_name(i)}")
+            device_list.append(i)
+        return device_list
 
+    def load_json(self, device):
+        
+        try:
+            with open(f"profiles/{device}.json", "r") as f:
+                prof = json.load(f)
+            print("Device MIDI data = ", prof)
+        except FileNotFoundError:
+            print("No profile for this device found")
 
-
+        return prof
 interface = KeysInterface()
